af490186246383939804235dedc40672b49167e6
Stefan van Herwijnen <stefan@devliert.info>
2025-04-04 13:42:32 +0200

    fix(preset): fix QRange

---
+++ packages/dev/src/pages/components/RangePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page padding>
+    <div class="row q-gutter-md">
+      <q-range v-model="model" :min="0" :max="50" />
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const model = ref({
+  min: 10,
+  max: 35
+})
+</script>
