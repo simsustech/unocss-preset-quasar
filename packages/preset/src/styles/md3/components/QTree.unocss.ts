044fa31f1b3d3756bb6efe5209c77af15812de18
Stefan van Herwijnen <stefan@devliert.info>
2025-04-08 15:46:15 +0200

    fix(preset): fix QTree

--- packages/preset/src/styles/md3/components/QTree.unocss.ts
+++ packages/preset/src/styles/md3/components/QTree.unocss.ts
@@ -13,32 +13,34 @@ const preflights: Preflight<QuasarTheme>[] = [
 `
   }
 ]
+
 const shortcuts: UserShortcuts<QuasarTheme> = [
   [
     /^q-tree$/,
     ([, c], { theme }) =>
-      theme.quasar?.components?.['q-tree'] ?? `relative text-[#9e9e9e]`
+      theme.quasar?.components?.['q-tree'] ??
+      `relative text-[#9e9e9e] [&_>_.q-tree\\_\\_node]:(p-0) [&_>_.q-tree\\_\\_node:after]:(hidden) [&_>_.q-tree\\_\\_node>_.q-tree\\_\\_node-header:before]:(hidden) [&_>_.q-tree\\_\\_node--child>_.q-tree\\_\\_node-header]:(pl-[24px])`
   ],
 
   [
     /^q-tree__node$/,
     ([, c], { theme }) =>
       theme.quasar?.components?.['q-tree__node'] ??
-      `pt-[0] pr-[0] pb-[3px] pl-[22px] [&:after]:(content-empty absolute -top-[3px] bottom-[0] w-[2px] right-auto -left-[13px] border-l-current) [&:last-child:after]:(hidden)`
+      `pt-[0] pr-[0] pb-[3px] pl-[22px] [&:after]:(content-empty absolute -top-[3px] bottom-[0] w-[2px] right-auto -left-[13px] border-left-current) [&:last-child:after]:(hidden)`
   ],
 
   [
     /^q-tree__node--disabled$/,
     ([, c], { theme }) =>
       theme.quasar?.components?.['q-tree__node--disabled'] ??
-      `pointer-events-none [&_.disabled]:(!opacity-100) [&_>_div]:(!opacity-60) [&_>_i]:(!opacity-60) [&_>_.disabled]:(!opacity-60) [&_.q-tree__node--disabled_>_div]:(!opacity-100) [&_.q-tree__node--disabled_>_i]:(!opacity-100) [&_.q-tree__node--disabled_>_.disabled]:(!opacity-100)`
+      `pointer-events-none [&_.disabled]:(!opacity-100) [&>div]:(!opacity-60) [&>i]:(!opacity-60) [&>_.disabled]:(!opacity-60) [&>div_.q-tree\\_\\_node--disabled>div]:(!opacity-100) [&>div_.q-tree\\_\\_node--disabled>i]:(!opacity-100) [&>div_.q-tree\\_\\_node--disabled>_.disabled]:(!opacity-100) [&>i_.q-tree\\_\\_node--disabled>div]:(!opacity-100) [&>i_.q-tree\\_\\_node--disabled>i]:(!opacity-100) [&>i_.q-tree\\_\\_node--disabled>_.disabled]:(!opacity-100) [&>_.disabled_.q-tree\\_\\_node--disabled>div]:(!opacity-100) [&>_.disabled_.q-tree\\_\\_node--disabled>i]:(!opacity-100) [&>_.disabled_.q-tree\\_\\_node--disabled>_.disabled]:(!opacity-100)`
   ],
 
   [
     /^q-tree__node-header$/,
     ([, c], { theme }) =>
       theme.quasar?.components?.['q-tree__node-header'] ??
-      `[&:before]:(content-empty absolute -top-[3px] bottom-2/4 w-[31px] -left-[35px] border-l-current border-b-current) p-[4px] mt-[3px] rounded-[4px] outline-[0]`
+      `[&:before]:(content-empty absolute -top-[3px] bottom-2/4 w-[31px] -left-[35px] border-left-current border-bottom-current) p-[4px] mt-[3px] rounded-[4px] outline-[0]`
   ],
 
   [
@@ -58,7 +60,7 @@ const shortcuts: UserShortcuts<QuasarTheme> = [
     /^q-tree__node--parent$/,
     ([, c], { theme }) =>
       theme.quasar?.components?.['q-tree__node--parent'] ??
-      `pl-[2px] [&_>_.q-tree__node-header:before]:(w-[15px] -left-[15px]) [&_>_.q-tree__node-collapsible_>_.q-tree__node-body]:(pt-[5px] pr-[0] pb-[8px] pl-[27px]) [&_>_.q-tree__node-collapsible_>_.q-tree__node-body:after]:(content-empty absolute top-[0] w-[2px] h-full right-auto left-[12px] border-l-current bottom-[50px])`
+      `pl-[2px] [&>_.q-tree__node-header:before]:(w-[15px] -left-[15px]) [&>_.q-tree__node-collapsible>_.q-tree__node-body]:(pt-[5px] pr-[0] pb-[8px] pl-[27px]) [&>_.q-tree__node-collapsible>_.q-tree__node-body:after]:(content-empty absolute top-[0] w-[2px] h-full right-auto left-[12px] border-left-current bottom-[50px])`
   ],
 
   [
@@ -71,21 +73,20 @@ const shortcuts: UserShortcuts<QuasarTheme> = [
     /^q-tree__node-header-content$/,
     ([, c], { theme }) =>
       theme.quasar?.components?.['q-tree__node-header-content'] ??
-      `text-[#000] transition-colors-300 [&_.q-icon]:(text-[${theme?.quasar?.components?.['q-tree__icon']?.fontSize ?? '21px'}]) [&_.q-avatar]:(text-[28px] rounded-50% w-[28px] h-[28px])`
+      `text-[#000] [transition:color_0.3s] [&_.q-icon]:(text-21px) [&_.q-avatar]:(text-[28px] rounded-[50%] w-[28px] h-[28px])`
   ],
 
   [
     /^q-tree__node--selected$/,
     ([, c], { theme }) =>
       theme.quasar?.components?.['q-tree__node--selected'] ??
-      `[&_.q-tree__node-header-content]:(text-[#9e9e9e])`
+      `[&_.q-tree\\_\\_node--selected_.q-tree__node-header-content]:(text-[#9e9e9e])`
   ],
 
   [
     /^q-tree__icon$/,
     ([, c], { theme }) =>
-      theme.quasar?.components?.['q-tree__icon'] ??
-      `text-[${theme?.quasar?.components?.['q-tree__icon']?.fontSize ?? '21px'}]`
+      theme.quasar?.components?.['q-tree__icon'] ?? `text-21px`
   ],
 
   [
@@ -98,14 +99,14 @@ const shortcuts: UserShortcuts<QuasarTheme> = [
     /^q-tree__avatar$/,
     ([, c], { theme }) =>
       theme.quasar?.components?.['q-tree__avatar'] ??
-      `text-[28px] rounded-50% w-[28px] h-[28px]`
+      `text-[28px] rounded-[50%] w-[28px] h-[28px]`
   ],
 
   [
     /^q-tree__arrow$/,
     ([, c], { theme }) =>
       theme.quasar?.components?.['q-tree__arrow'] ??
-      `text-[16px] mr-[4px] transition-transform-300`
+      `text-[16px] mr-[4px] [transition:transform_0.3s]`
   ],
 
   [
@@ -118,7 +119,7 @@ const shortcuts: UserShortcuts<QuasarTheme> = [
     /^q-tree__arrow--rotate$/,
     ([, c], { theme }) =>
       theme.quasar?.components?.['q-tree__arrow--rotate'] ??
-      `[transform:rotate3d(0,0,1,90deg)]`
+      `[transform:rotate3d(0_0_1_90deg)]`
   ],
 
   [
@@ -131,28 +132,28 @@ const shortcuts: UserShortcuts<QuasarTheme> = [
     /^q-tree--dark$/,
     ([, c], { theme }) =>
       theme.quasar?.components?.['q-tree--dark'] ??
-      `[&_.q-tree__node-header-content]:(text-[#fff])`
+      `[&--dark] [&_.q-tree__node-header-content]:(text-[#fff])`
   ],
 
   [
     /^q-tree--no-connectors$/,
     ([, c], { theme }) =>
       theme.quasar?.components?.['q-tree--no-connectors'] ??
-      `[&_.q-tree__node:after]:(!hidden) [&_.q-tree__node-header:before]:(!hidden) [&_.q-tree__node-body:after]:(!hidden)`
+      `[&--no-connectors] [&_.q-tree__node:after]:(!hidden) [&--no-connectors] [&_.q-tree__node-header:before]:(!hidden) [&--no-connectors] [&_.q-tree__node-body:after]:(!hidden)`
   ],
 
   [
     /^q-tree--dense$/,
     ([, c], { theme }) =>
       theme.quasar?.components?.['q-tree--dense'] ??
-      `[&_>_.q-tree__node--child_>_.q-tree__node-header]:(pl-px) [&_.q-tree__arrow]:(mr-px) [&_.q-tree__spinner]:(mr-px) [&_.q-tree__img]:(h-[32px]) [&_.q-tree__tickbox]:(mr-[3px]) [&_.q-tree__node]:(p-0) [&_.q-tree__node:after]:(top-[0] -left-[8px]) [&_.q-tree__node-header]:(mt-0 p-px) [&_.q-tree__node-header:before]:(top-[0] -left-[8px] w-[8px]) [&_.q-tree__node--child]:(pl-[17px]) [&_.q-tree__node--child_>_.q-tree__node-header:before]:(-left-[25px] w-[21px]) [&_.q-tree__node-body]:(pt-[0] pr-[0] pb-[2px] pl-[0]) [&_.q-tree__children]:(pl-[16px])`
+      `[&--dense] &>_.q-tree__node--child>_.q-tree__node-header:(pl-px) [&--dense] [&_.q-tree__arrow]:(mr-px) [&--dense] [&_.q-tree__spinner]:(mr-px) [&--dense] [&_.q-tree__img]:(h-[32px]) [&--dense] [&_.q-tree__tickbox]:(mr-[3px]) [&--dense] [&_.q-tree__node]:(p-0) [&--dense] [&_.q-tree__node:after]:(top-[0] -left-[8px]) [&--dense] [&_.q-tree__node-header]:(mt-0 p-px) [&--dense] [&_.q-tree__node-header:before]:(top-[0] -left-[8px] w-[8px]) [&--dense] &>_.q-tree__node--child>_.q-tree__node-header:(pl-[17px]) [&--dense] &>_.q-tree__node--child>_.q-tree__node-header:before:(-left-[25px] w-[21px]) [&--dense] [&_.q-tree__node-body]:(pt-[0] pr-[0] pb-[2px]) [&--dense] [&_.q-tree__children]:(pl-[16px])`
   ],
 
   [
     /^q-tree--dense$/,
     ([, c], { theme }) =>
       theme.quasar?.components?.['q-tree--dense'] ??
-      `[&__.q-tree__node--parent__>_.q-tree__node-collapsible__>_.q-tree__node-body]:(pt-[0] pr-[0] pb-[2px] pl-[20px]) [&__.q-tree__node--parent__>_.q-tree__node-collapsible__>_.q-tree__node-body:after]:(left-[8px])`
+      `[&--dense] &__.q-tree__node--parent>_.q-tree__node-collapsible>_.q-tree__node-body:(pt-[0] pr-[0] pb-[2px] pl-[20px]) [&--dense] &__.q-tree__node--parent>_.q-tree__node-collapsible>_.q-tree__node-body:after:(left-[8px])`
   ]
 ]
 
