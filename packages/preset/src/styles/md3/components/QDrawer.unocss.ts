8b5e71d4cd95b5243281ff258b3799877ea0936a
Stefan van Herwijnen <stefan@devliert.info>
2025-04-09 14:20:55 +0200

    fix(preset): fix QDrawer q-router-links

--- packages/preset/src/styles/md3/components/QDrawer.unocss.ts
+++ packages/preset/src/styles/md3/components/QDrawer.unocss.ts
@@ -85,8 +85,8 @@ const shortcuts: UserShortcuts<QuasarTheme> = [
       theme.quasar?.components?.['q-drawer__content'] ??
       `[&_>_.q-list]:(px-28px)
       [&_>_.q-list_>_.q-item]:(border-rd-16px)
-      [&_>_.q-list_>_.q-router-link--active]:(text-light-primary dark:text-dark-primary)
-      [&_>_.q-list_>_.q-router-link--exact-active]:(bg-light-secondary-container dark:bg-dark-secondary-container
+      [&_.q-list_>_.q-router-link--active]:(text-light-primary dark:text-dark-primary)
+      [&_.q-list_>_.q-router-link--exact-active]:(bg-light-secondary-container dark:bg-dark-secondary-container
       )`
   ]
 ]
