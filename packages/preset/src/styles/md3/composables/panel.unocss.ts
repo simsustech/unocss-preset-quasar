a721809a9469bea4331428ebe5b6a02b69d9f14d
Stefan van Herwijnen <stefan@devliert.info>
2025-05-20 11:33:09 +0200

    fix(preset): fix q-panel

--- packages/preset/src/styles/md3/composables/panel.unocss.ts
+++ packages/preset/src/styles/md3/composables/panel.unocss.ts
@@ -2,7 +2,7 @@ import type { UserShortcuts } from '@unocss/core'
 import type { QuasarTheme } from '../../../theme.js'
 
 const shortcuts: UserShortcuts<QuasarTheme> = [
-  [/^q-panel$/, ([, c], { theme }) => `h-full w-full [&_div]:(h-full w-full)`],
+  [/^q-panel$/, ([, c], { theme }) => `h-full w-full [&>div]:(h-full w-full)`],
   [/^q-panel-parent$/, ([, c], { theme }) => `overflow-hidden relative`]
 ]
 
