b5d257230d5f2fe7053b6754e672b7e551e8beb3
Stefan van Herwijnen <stefan@devliert.info>
2026-07-02 22:11:51 +0200

    fix(preset): QTooltip unify line-height to leading-none

--- packages/preset/src/styles/md3/components/QTooltip.unocss.ts
+++ packages/preset/src/styles/md3/components/QTooltip.unocss.ts
@@ -10,7 +10,7 @@ const shortcuts: Shortcut<QuasarTheme>[] = [
     componentCtxClass(
       'q-tooltip--style',
       ({ theme }) =>
-        `text-[10px] leading-[24px] bg-$light-inverse-surface dark:bg-$dark-inverse-surface text-$light-inverse-on-surface dark:text-$dark-inverse-on-surface rounded-$shape-corner-small normal-case font-normal pointer-events-none`
+        `text-[10px] leading-none bg-$light-inverse-surface dark:bg-$dark-inverse-surface text-$light-inverse-on-surface dark:text-$dark-inverse-on-surface rounded-$shape-corner-small normal-case font-normal pointer-events-none`
     )
   ],
   [
