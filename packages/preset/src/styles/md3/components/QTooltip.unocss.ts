e6a256dc0ef964de8ba0d5abe54ddebb7808edd3
Stefan van Herwijnen <stefan@devliert.info>
2026-07-02 22:07:15 +0200

    fix(preset): QTooltip overflow-visible — overflow-y:auto breaks flex centering

--- packages/preset/src/styles/md3/components/QTooltip.unocss.ts
+++ packages/preset/src/styles/md3/components/QTooltip.unocss.ts
@@ -12,6 +12,14 @@ const shortcuts: Shortcut<QuasarTheme>[] = [
       ({ theme }) =>
         `!flex !items-center text-[12px] leading-[16px] bg-$light-inverse-surface dark:bg-$dark-inverse-surface text-$light-inverse-on-surface dark:text-$dark-inverse-on-surface rounded-$shape-corner-small normal-case font-normal pointer-events-none`
     )
+  ],
+  [
+    /^q-tooltip$/,
+    componentCtxClass(
+      'q-tooltip',
+      ({ theme }) =>
+        `z-${theme.quasar.z['tooltip']} !fixed overflow-visible px-[10px] py-[6px] max-w-[95vw]`
+    )
   ]
 ]
 
